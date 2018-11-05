53651c3092adf0e0b232ffbab29d0f193ef6897f
Pri <priyankachhetri@gmail.com>
2018-11-05 18:11:53 -0500

    New Changes to Join
    fixed

--- app/src/main/java/com/example/ariana/scavenger/JoinHunt.java
+++ app/src/main/java/com/example/ariana/scavenger/JoinHunt.java
@@ -13,8 +13,6 @@ public class JoinHunt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_hunt);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
     }
